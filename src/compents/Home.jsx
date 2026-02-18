0be7f24aa3a581186d50559cde7b9007a2ce9b2f
Foxmanu <sjm7008@gmail.com>
2026-02-18 11:25:18 +0530

    deeleteing the  detial

--- src/compents/Home.jsx
+++ src/compents/Home.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import Detail from './Detail';
+
 
 
 function Home() {
